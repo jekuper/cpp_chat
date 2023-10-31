2ee1bca11092512aee26003b015306081f197f0a
Jekuper <jekkyper1121@gmail.com>
2023-10-30 21:45:22 -0400

    implemented base

--- cpp_chat_client/cpp_chat_client/Client.cpp
+++ cpp_chat_client/cpp_chat_client/Client.cpp
@@ -87,8 +87,10 @@ int main(int argc, char* argv[]) {
 	do {
 
 		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
-		if (iResult > 0)
+		if (iResult > 0) {
 			printf("Bytes received: %d\n", iResult);
+			printf("Message: %.*s\n", iResult, recvbuf);
+		}
 		else if (iResult == 0)
 			printf("Connection closed\n");
 		else
--- cpp_chat_server/cpp_chat_server/Server.cpp
+++ cpp_chat_server/cpp_chat_server/Server.cpp
@@ -90,6 +90,7 @@ int main()
 		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
 		if (iResult > 0) {
 			printf("Bytes received: %d\n", iResult);
+			printf("Message: %.*s\n", iResult, recvbuf);
 
 			// Echo the buffer back to the sender
 			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
